f48bcbb60c8811b78c8d8f0de4fc977f2d402f82
hublad <hublad@wp.pl>
2020-09-09 16:32:20 +0200

    Desktop view added for slide 5

--- src/components/TheSlide5Galaxy.vue
+++ src/components/TheSlide5Galaxy.vue
@@ -56,8 +56,8 @@ export default {
     ".    time time time time time"
     ".    time time time time time"
     ".    time time time time time"
-    "bang bang .    .    .    .  "
-    "bang bang .    cap  cap  cap"
+    "bang bang bang    .    .    .  "
+    "bang bang bang    cap  cap  cap"
     "img  img  img  img  img  img"
     "img  img  img  img  img  img"
     "img  img  img  img  img  img"
@@ -76,26 +76,48 @@ export default {
     ".     .     .     fil2  fil2  fil2  "
     ".     .     .     .     .     .     ";
 
+  @media screen and (min-width: 700px) {
+    grid-template-rows: repeat(16, 6.5vw);
+    grid-template-areas:
+      ".   .   .    .    .  time   time  time  time  time   "
+      "cap cap cap  cap  .  time   time  time  time  time "
+      "img img img  img  img  img  bang bang  bang  bang    "
+      "img img img  img  img  img  bang bang  bang  bang    "
+      "img img img  img  img  img  bang bang  bang  bang    "
+      "img img img  img  img  img  fil  .     .     .       "
+      "text  text text text text   text  text text text text"
+      "text  text text text text   text  text text text text"
+      ".     .     .   .    .      .     .    .    .    .   "
+      ".     .     .   .    .      .     .    .    .    .   "
+      "stars stars stars stars stars stars stars stars stars stars"
+      "stars stars stars stars stars stars stars stars stars stars"
+      ".     .     .     .     .     .     fil2  fil2  fil2  fil2"
+      "text2 text2 text2 text2 text2 text2 fil2  fil2  fil2  fil2"
+      "text2 text2 text2 text2 text2 text2 .     .     .     .   "
+      ".     .     .     .     .     .     .     .     .     .   ";
+  }
+
   &__tile-time {
     grid-area: time;
     /* @extend %tile-gray-header; */
-    @extend %tile-white-header;
+    /* @extend %tile-white-header; */
     /* @extend %tile-white; */
     /* @extend %tile-gray; */
+    @extend %tile-white-time;
   }
 
   &__tile-bang {
     grid-area: bang;
-    /* @extend %tile-gray-header; */
+    @extend %tile-gray-header;
     /* @extend %tile-white-header; */
     /* @extend %tile-white; */
-    @extend %tile-gray;
-    font-size: 5.3vw;
+    @media screen and (max-width: 700px) {
+      font-size: 7vw;
+    }
   }
 
   &__tile-caption {
     grid-area: cap;
-    font-size: 5vw;
     @extend %tile-white;
     /* @extend %tile-purple; */
   }
