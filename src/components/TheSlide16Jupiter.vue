91f4c6c1f88945e0a1ba602ef17b854c93cb2a5d
hublad <hublad@wp.pl>
2020-09-11 14:20:35 +0200

    Added slide 16 desktop view

--- src/components/TheSlide16Jupiter.vue
+++ src/components/TheSlide16Jupiter.vue
@@ -145,9 +145,33 @@ export default {
     ".    but  but  but  but  .     "
     ".    .    .    .    .    .     ";
 
+  @media screen and (min-width: 700px) {
+    grid-template-rows: repeat(20, 6.5vw);
+    grid-template-areas:
+      ".    .    .   .   .    .    cap  cap  cap cap "
+      "img  img  img img img  img  img  img  img img "
+      "img  img  img img img  img  img  img  img img "
+      "img  img  img img img  img  img  img  img img "
+      "img  img  img img img  img  img  img  img img "
+      "img  img  img img img  img  img  img  img img "
+      "text text text text text text text text2 text2 text2"
+      "text text text text text text text text2 text2 text2"
+      "text text text text text text text text2 text2 text2"
+      "text text text text text text text .     .     .    "
+      ".    .    .    .    .    .    .    .    .    .   "
+      "capa capa capa capa .    .    .    .    .    .   "
+      "imga  imga  imga  imga imga   imga imga  imga  imga  imga "
+      "imga  imga  imga  imga imga   imga imga  imga  imga  imga "
+      "imga  imga  imga  imga imga   imga imga  imga  imga  imga "
+      "imga  imga  imga  imga imga   imga imga  imga  imga  imga "
+      "imga  imga  imga  imga imga   imga imga  imga  imga  imga "
+      ".    .    .    .    .    .    .    .    .    .   "
+      ".    .    .    but  but  but  but  .    .    .   "
+      ".    .    .    .    .    .    .    .    .    .   ";
+  }
+
   &__caption {
     grid-area: cap;
-    font-size: 5vw;
     @extend %tile-white;
     /* @extend %tile-purple; */
   }
@@ -177,7 +201,6 @@ export default {
 
   &__caption-aside {
     grid-area: capa;
-    font-size: 5vw;
     @extend %tile-purple;
   }
 
@@ -201,7 +224,6 @@ export default {
       width: 100%;
       height: 100%;
       @extend %button;
-      font-size: 5vw;
     }
   }
 }
